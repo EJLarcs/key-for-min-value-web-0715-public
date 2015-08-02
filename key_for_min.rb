9cb5ba488c837f223002f68185f6bff7841e15d5
EJLarcs <ejlarcombe@gmail.com>
2015-08-02 15:42:16 -0400

    key min value

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  array = []
+  name_hash.each_pair do |k, v|
+      array << v
+  end
+  name_hash.key(array.sort.shift)
 end
+
+
